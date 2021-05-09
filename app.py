6f9cc06fe4a3e76c9cac36d3a0a19a1ed6feeffe
sabuchen <t108ac8514@ntut.org.tw>
2021-05-10 01:04:09 +0800

    viewpoint update

--- app.py
+++ app.py
@@ -173,16 +173,16 @@ def sendViewPoint(event):  #景點查詢的轉盤樣板
                     CarouselColumn(
                         thumbnail_image_url='https://raw.githubusercontent.com/sabucchen/pic/main/%E9%99%B3%E5%A4%A9%E4%BE%86%E6%95%85%E5%B1%85.jpg',
                         title='古蹟介紹',
-                        # text='奶素',
+                        text='古蹟介紹',
                         actions=MessageTemplateAction(
                             label='了解更多',
                             text='了解更多'
                         ),
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://www.taipeileechi.com.tw/ProductCategory/ProductUnit20140606193619.jpg',
+                        thumbnail_image_url='https://raw.githubusercontent.com/sabucchen/pic/main/%E5%8F%B0%E5%8C%97%E9%9C%9E%E6%B5%B7%E5%9F%8E%E9%9A%8D%E5%BB%9F.jpg',
                         title='歷史廟宇',
-                        # text='蛋奶素',
+                        text='歷史廟宇',
                         actions=MessageTemplateAction(
                             label='了解更多',
                             text='了解更多'
