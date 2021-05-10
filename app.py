a51b16fb8b45f6970adc52e39a3330f89fe4e21a
sabuchen <t108ac8514@ntut.org.tw>
2021-05-10 20:13:30 +0800

    again

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def sendViewPoint(event):  #景點的轉盤樣板
                                 uri='https://my-parpertest.web.app/hello/hello.html'
                             ),
                             PostbackTemplateAction(
-                                gilabel='回傳訊息四',
+                                label='回傳訊息四',
                                 data='action=sell&item=飲料'
                             ),
                         ]
