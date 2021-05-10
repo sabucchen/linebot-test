cf0ed23216fa48cc2fe59c21a9cd3d25c77e1698
sabuchen <t108ac8514@ntut.org.tw>
2021-05-10 08:58:23 +0800

    try del button

--- app.py
+++ app.py
@@ -184,16 +184,35 @@ def sendViewPoint(event):  #景點的轉盤樣板
                                 label='VR虛擬導覽',
                                 uri='http://www.e-happy.com.tw'
                             ),
-                            PostbackTemplateAction(
-                                label='回傳訊息一',
-                                data='action=sell&item=披薩'
-                            ),
+                            # PostbackTemplateAction(
+                            #     label='回傳訊息一',
+                            #     data='action=sell&item=披薩'
+                            # ),
                         ]
                     ),
                     CarouselColumn(
                         thumbnail_image_url='https://raw.githubusercontent.com/sabucchen/pic/main/%E5%8F%B0%E5%8C%97%E9%9C%9E%E6%B5%B7%E5%9F%8E%E9%9A%8D%E5%BB%9F.jpg',
                         title='歷史廟宇',
                         text='歷史廟宇',
+                        actions=[
+                            MessageTemplateAction(
+                                label='了解更多',
+                                text='了解更多'
+                            ),
+                            URITemplateAction(
+                                label='VR虛擬導覽',
+                                uri='http://www.ntu.edu.tw'
+                            ),
+                            PostbackTemplateAction(
+                                label='回傳訊息二',
+                                data='action=sell&item=飲料'
+                            ),
+                        ]
+                    )
+                    CarouselColumn(
+                        thumbnail_image_url='https://raw.githubusercontent.com/sabucchen/pic/main/%E5%8F%B0%E5%8C%97%E9%9C%9E%E6%B5%B7%E5%9F%8E%E9%9A%8D%E5%BB%9F.jpg',
+                        title='特色商行',
+                        text='特色商行',
                         actions=[
                             MessageTemplateAction(
                                 label='了解更多',
